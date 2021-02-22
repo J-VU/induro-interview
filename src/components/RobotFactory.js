4546ab1f8369258b5bafed751b9b2a4e9412aa44
ME Vu <jvu@prometheus8.com>
2021-02-21 19:42:11 -0500

    add card

--- src/components/RobotFactory.js
+++ src/components/RobotFactory.js
@@ -35,77 +35,79 @@ const RobotFactory = () => {
       console.error(error.message)
     }
   }
-  
+
   return (
     <Fragment>
       <form onSubmit={(event) => onSubmit(event)}>
         <div className='card p-3'>
           <p className='title'>Robot Factory</p>
-          <div className='field'>
-            <label className='label'>Name</label>
-            <div className='control'>
-              <input
-                className='input'
-                type='text'
-                placeholder='Generic Robot'
-                name='name'
-                value={name}
-                onChange={(event) => onChange(event)}
-                required
-              />
-            </div>
-          </div>
-          <div className='field'>
-            <label className='label'>Color</label>
-            <div className='control'>
-              <div className='select'>
-                <select
-                  name='color'
-                  value={color}
+          <div className='card p-2 has-background-light'>
+            <div className='field'>
+              <label className='label'>Name</label>
+              <div className='control'>
+                <input
+                  className='input'
+                  type='text'
+                  placeholder='Generic Robot'
+                  name='name'
+                  value={name}
                   onChange={(event) => onChange(event)}
-                  required>
-                  <option value='None'>None</option>
-                  <option value='Red'>Red</option>
-                  <option value='Green'>Green</option>
-                  <option value='Blue'>Blue</option>
-                  <option value='Black'>Black</option>
-                  <option value='White'>White</option>
-                  <option value='RainbowRGB'>RainbowRGB</option>
-                </select>
+                  required
+                />
               </div>
             </div>
-          </div>
-          <div className='field'>
-            <label className='label'>Attack</label>
-            <div className='control'>
-              <input
-                className='input'
-                type='number'
-                placeholder='100'
-                name='attack'
-                value={attack}
-                onChange={(event) => onChange(event)}
-                required
-              />
+            <div className='field'>
+              <label className='label'>Color</label>
+              <div className='control'>
+                <div className='select'>
+                  <select
+                    name='color'
+                    value={color}
+                    onChange={(event) => onChange(event)}
+                    required>
+                    <option value='None'>None</option>
+                    <option value='Red'>Red</option>
+                    <option value='Green'>Green</option>
+                    <option value='Blue'>Blue</option>
+                    <option value='Black'>Black</option>
+                    <option value='White'>White</option>
+                    <option value='RainbowRGB'>RainbowRGB</option>
+                  </select>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className='field'>
-            <label className='label'>Defense</label>
-            <div className='control'>
-              <input
-                className='input'
-                type='number'
-                placeholder='100'
-                name='defense'
-                value={defense}
-                onChange={(event) => onChange(event)}
-                required
-              />
+            <div className='field'>
+              <label className='label'>Attack</label>
+              <div className='control'>
+                <input
+                  className='input'
+                  type='number'
+                  placeholder='100'
+                  name='attack'
+                  value={attack}
+                  onChange={(event) => onChange(event)}
+                  required
+                />
+              </div>
             </div>
-          </div>
-          <div className='field is-grouped'>
-            <div className='control'>
-              <button className='button is-link'>Submit</button>
+            <div className='field'>
+              <label className='label'>Defense</label>
+              <div className='control'>
+                <input
+                  className='input'
+                  type='number'
+                  placeholder='100'
+                  name='defense'
+                  value={defense}
+                  onChange={(event) => onChange(event)}
+                  required
+                />
+              </div>
+            </div>
+            <div className='field is-grouped'>
+              <div className='control'>
+                <button className='button is-link'>Submit</button>
+              </div>
             </div>
           </div>
         </div>
